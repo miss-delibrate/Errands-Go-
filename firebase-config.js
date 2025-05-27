ac17a6e56b8492e2e55ee72108cf2c77e7c3d049
miss-delibrate <missdeliberate@gmail.com>
2025-05-27 11:15:32 +0000

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2UXzy9qBLmFquWFEH8kgU64MuuYslxm8",
+  authDomain: "errand-go-31902.firebaseapp.com",
+  projectId: "errand-go-31902",
+  storageBucket: "errand-go-31902.appspot.com",
+  messagingSenderId: "196036183828",
+  appId: "1:196036183828:web:77fbebf7fd63e940346401",
+};
+
+firebase.initializeApp(firebaseConfig);
