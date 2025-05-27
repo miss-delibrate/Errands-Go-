3a03cf209078fcfeb63966a26db8541431859b36
miss-delibrate <missdeliberate@gmail.com>
2025-05-27 11:15:02 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+function startAsSender() {
+  alert("Sender flow starting...");
+}
+function startAsRunner() {
+  alert("Runner flow starting...");
+}
+function showLogin() {
+  document.querySelector('.login').style.display = 'block';
+}
+function showDashboard() {
+  alert("Dashboard page (under construction)");
+}
